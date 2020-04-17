dd4bc5f2034d2b24223ca6cc5c465eca22905d36
Ihor Kobyzhcha <igorkobyzhcha@gmail.com>
2020-04-17 16:15:10 +0300

    Updates

---
+++ precache-manifest.3e688c816db5a6c772e1c93f384e8dc5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce3858458f5bb0e109c8b3fa8ef0660",
+    "url": "/jorris/index.html"
+  },
+  {
+    "revision": "06bdd45bc9c7d6727f80",
+    "url": "/jorris/static/css/main.fa69802e.chunk.css"
+  },
+  {
+    "revision": "0c07336bdac98c767315",
+    "url": "/jorris/static/js/2.0ce17969.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jorris/static/js/2.0ce17969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06bdd45bc9c7d6727f80",
+    "url": "/jorris/static/js/main.751668bd.chunk.js"
+  },
+  {
+    "revision": "180e2d7d52c712348b74",
+    "url": "/jorris/static/js/runtime-main.a515f899.js"
+  },
+  {
+    "revision": "d247cf9d11903d4c4020bd531c4b14c7",
+    "url": "/jorris/static/media/comment.d247cf9d.svg"
+  },
+  {
+    "revision": "e977fb9e726c00e0765ae53cc9a0dd66",
+    "url": "/jorris/static/media/like.e977fb9e.svg"
+  },
+  {
+    "revision": "0865c8322b3cc6f1bed5a8f1f35f00b5",
+    "url": "/jorris/static/media/liked.0865c832.svg"
+  },
+  {
+    "revision": "f8a998d1878c0acd21eab998d9eead5c",
+    "url": "/jorris/static/media/link.f8a998d1.svg"
+  }
+]);
